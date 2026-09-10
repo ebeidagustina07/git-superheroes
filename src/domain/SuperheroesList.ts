9b66e2ea0b3e22ed5cf4052888404e5e33f1664f
Agustina Ebeid <ebeidagustina07@gmail.com>
2026-09-10 10:41:26 -0300

    add hero

--- src/domain/SuperheroesList.ts
+++ src/domain/SuperheroesList.ts
@@ -8,6 +8,8 @@ export class SuperheroesList {
     { name: "Thanos", side: "villain" },
     { name: "Loki", side: "villain" },
     { name: "Wanda", side: "villain" },
+    { name: "sofi", side: "villain" },
+    { name: "agus", side: "hero" },
   ];
 
   getHeroes(): Superhero[] {
